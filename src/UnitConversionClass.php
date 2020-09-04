f200181d0ef74ca11d57505b3ea399397dc6c5ed
EyadHamza <EyadHamza0@outlook.com>
2020-09-04 03:17:50 +0200

    convert kilos to lbs

--- src/UnitConversionClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Theprofessor\UnitConversion;
-
-class UnitConversionClass
-{
-}
---
+++ src/Weight.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Theprofessor\UnitConversion;
+
+class Weight
+{
+    private float $kilograms;
+
+    public static function fromKilograms(float $kilograms)
+    {
+        return new static($kilograms);
+    }
+    public function __construct(float $kilograms)
+    {
+
+        $this->kilograms = $kilograms;
+    }
+    public function toLbs() :float
+    {
+        return $this->kilograms*2.204623;
+    }
+}
